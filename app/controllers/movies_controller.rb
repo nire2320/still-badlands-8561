b4396b4fae3d6dcf5e17379c5feca6a3118d3cd8
Nicholas Reynders <gggorillaz@gmail.com>
2014-02-26 04:12:43 -0400

    updating heroku stuff

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
 
+  @@sort_var = 0
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +9,38 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    #debugger
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      #sort_var = params[:sort_var].to_i
+      #@@sort_var = (@@sort_var - sort_var).abs
+      #if @@sort_var == 0
+        ordering, @title_header = {:order => 'title ASC'}, 'hilite'
+      #else
+      #  ordering, @title_header = {:order => 'title DESC'}, 'hilite'
+      #end
+    when 'release_date'
+      #if @date_header == nil
+        ordering, @date_header = {:order => 'release_date ASC'}, 'hilite'
+      #else
+      #  ordering, @date_header = {:order => 'release_date DESC'}, 'hilite'
+      #end
+    end
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+
+    #fills the ratings initially when webpage is loaded
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    end
+
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
   end
 
   def new
